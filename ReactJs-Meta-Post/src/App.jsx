b20a2cc1e8883031989931968543f2c5ba80de01
mohammed kaif <mkaif555123@gmail.com>
2024-08-28 16:08:29 +0500

    5% work done

--- ReactJs-Meta-Post/src/App.jsx
+++ ReactJs-Meta-Post/src/App.jsx
@@ -3,8 +3,12 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        
+      <div className="bg-white w-40 min-h-max text-center m-auto mt-16">
+         
+         <div>
+          <h1>Meta Post</h1>
+         </div>
+     
       </div>
     </>
   );
