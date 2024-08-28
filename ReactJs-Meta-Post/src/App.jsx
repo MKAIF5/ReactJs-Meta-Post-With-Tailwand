a55cc689b4ad7f211834dd451fcc363d7f4cc363
mohammed kaif <mkaif555123@gmail.com>
2024-08-28 18:17:18 +0500

    project build start

--- ReactJs-Meta-Post/src/App.jsx
+++ ReactJs-Meta-Post/src/App.jsx
@@ -1,17 +1,17 @@
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <>
       <div className="bg-white w-40 min-h-max text-center m-auto mt-16">
-         
-         <div>
-          <h1>Meta Post</h1>
-         </div>
-     
+        <div>
+          <div className="left"></div>
+          <div className="right"></div>
+        </div>
       </div>
     </>
   );
 }
-
 export default App;
