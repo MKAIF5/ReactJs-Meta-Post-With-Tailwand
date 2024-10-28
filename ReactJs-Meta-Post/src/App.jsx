fc068509abaad7446979fe5f60e36d03dd667dc3
mohammed kaif <mkaif555123@gmail.com>
2024-10-28 14:57:20 +0500

    convert into components

--- ReactJs-Meta-Post/src/App.jsx
+++ ReactJs-Meta-Post/src/App.jsx
@@ -1,82 +1,10 @@
+import Meta from "./Components/Meta";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="bg-white w-1/3 m-auto mt-10 p-5">
-        <div className="flex justify-around gap-44">
-          <div className="flex gap-3">
-            <img
-              className="w-14 rounded-full"
-              src="https://avatars.githubusercontent.com/u/146006914?v=4"
-              alt=""
-            />
-            <div className="mt-3">
-              <p className="text-sm whitespace-nowrap">Mohammad Kaif</p>
-              <p className="text-sm whitespace-nowrap text-gray-500">
-                Wednesday at 8:00 PM
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-1">
-            <div className="w-2 h-2 bg-gray-500 rounded-full"></div>
-            <div className="w-2 h-2 bg-gray-500 rounded-full"></div>
-            <div className="w-2 h-2 bg-gray-500 rounded-full"></div>
-          </div>
-        </div>
-        <br />
-        <div className="paragraph">
-          <p className="text-black">
-            I Got The First Position In The Teckno Kids Course And Achieve The
-            Certificate With SmartWatch Of Saylani Mass It Training Program.
-            <span>
-              Sir Rizwan, thank you for treating me and the entire class so well
-              Learning
-            </span>
-          </p>
-          <br />
-          <p className="text-sm text-blue-500">
-            #Rizwan Bhatti #Saylani Mass It Training Program
-          </p>
-        </div>
-        <br />
-        <div>
-          <img className="w-auto h-1/4" src="../assets/my-awards.png" alt="" />
-        </div>
-        <br />
-        <div className="flex gap-16">
-          <div className="flex gap-2">
-            <p className="text-xl">❤️ 👍🏻 😂</p>
-            <p className=" text-gray-600 text-sm mt-2">
-              Imran and 300,000 others
-            </p>
-          </div>
-          <div className="right">
-            <p className=" text-gray-600 text-xs mt-2">300 Comments</p>
-          </div>
-        </div>
-        <br />
-        <hr className="border-gray-500" />
-        <br />
-        <div className="flex gap-16">
-          <div className="flex">
-            <img className="w-14 h-14" src="https://media.istockphoto.com/id/1136351242/vector/like-social-media-hand-line-icon-editable-stroke-pixel-perfect-for-mobile-and-web.jpg?s=612x612&w=0&k=20&c=OnKkq5JCHkLvZ1Ck_njtTQMCyLljXsXGNhGqVpwwVUA=" alt="" />
-            <p className="mt-5 text-sm">Like</p>
-          </div>
-          <div className="mt-4 flex gap-2">
-            <img className="w-8 h-8" src="https://cdn-icons-png.flaticon.com/512/1380/1380338.png" alt="" />
-            <p className=" mt-1 text-sm">Comment</p>
-          </div>
-          <div className="flex gap-1">
-            <img
-              className="w-10 h-10 mt-2"
-              src="https://cdn-icons-png.flaticon.com/512/2615/2615680.png"
-              alt=""
-            />
-            <p className="mt-4 text-sm">Share</p>
-          </div>
-        </div>
-      </div>
+      <Meta />
     </>
   );
 }
